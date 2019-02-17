a60aa4fb12f22f5bb7fb3597d32dadf9f71684d8
Dmytro Khmelenko <d.khmelenko@gmail.com>
2019-02-17 17:56:35 +0100

    Add dummy for unit tests

--- rxsmartlock/src/test/java/com/freeletics/rxsmartlock/ExampleUnitTest.kt
+++ rxsmartlock/src/test/java/com/freeletics/rxsmartlock/ExampleUnitTest.kt
@@ -4,11 +4,55 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-
 class SmartLockManagerTest {
 
     @Test
     fun storeCredentialsSuccessful() {
         // TODO Implement more tests
+    }
+
+    @Test
+    fun storeCredentialsFailed() {
+        // TODO
+    }
+
+    @Test
+    fun retrieveCredentialsSuccessful() {
+        // TODO
+    }
+
+    @Test
+    fun retrieveCredentialsFailed() {
+        // TODO
+    }
+
+    @Test
+    fun retrieveHintsSuccessful() {
+        // TODO
+    }
+
+    @Test
+    fun retrieveHintsFailed() {
+        // TODO
+    }
+
+    @Test
+    fun deleteHintsSuccessful() {
+        // TODO
+    }
+
+    @Test
+    fun deleteHintsFailed() {
+        // TODO
+    }
+
+    @Test
+    fun disableSmartLockSuccessful() {
+        // TODO
+    }
+
+    @Test
+    fun disableSmartLockFailed() {
+        // TODO
     }
 }
