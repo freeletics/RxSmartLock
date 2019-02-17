b0cbd961a0295ecd144d2b78f86658f3292e088d
Dmytro Khmelenko <d.khmelenko@gmail.com>
2019-02-17 16:37:15 +0100

    Clean up

---
+++ rxsmartlock/src/test/java/com/freeletics/rxsmartlock/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.freeletics.rxsmartlock
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class SmartLockManagerTest {
+
+    @Test
+    fun storeCredentialsSuccessful() {
+        // TODO Implement more tests
+    }
+}
